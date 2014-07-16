4fe4e026f97133f3a2370fef2c85c828d452fccf
John Kilpatrick <john@harkable.com>
2014-07-16 16:19:59 +0100

    all the flashly lights

---
+++ pi/controllers/piController.js
@@ -0,0 +1,99 @@
+var gpio = require("pi-gpio"),
+
+module.exports.twitter = function() {
+
+    setInterval(function() {
+
+        gpio.open(16, "output", function(err) {
+            gpio.write(16, 1, function() {
+                gpio.close(16);
+            });
+        });
+
+        gpio.open(18, "output", function(err) {
+            gpio.write(18, 1, function() {
+                gpio.close(18);
+            });
+        });
+
+    }, 10000);
+
+    gpio.open(16, "output", function(err) {
+        gpio.write(16, 0, function() {
+            gpio.close(16);
+        });
+    });
+
+    gpio.open(18, "output", function(err) {
+        gpio.write(18, 0, function() {
+            gpio.close(18);
+        });
+    });
+
+});
+
+module.exports.facebook = function() {
+
+    setInterval(function() {
+
+        gpio.open(23, "output", function(err) {
+            gpio.write(23, 1, function() {
+                gpio.close(23);
+            });
+        });
+
+        gpio.open(24, "output", function(err) {
+            gpio.write(24, 1, function() {
+                gpio.close(24);
+            });
+        });
+
+    }, 10000);
+
+    gpio.open(23, "output", function(err) {
+        gpio.write(23, 0, function() {
+            gpio.close(23);
+        });
+    });
+
+    gpio.open(24, "output", function(err) {
+        gpio.write(24, 0, function() {
+            gpio.close(24);
+        });
+    });
+
+
+});
+
+module.exports.dribble = function() {
+
+    setInterval(function() {
+
+        gpio.open(5, "output", function(err) {
+            gpio.write(5, 1, function() {
+                gpio.close(5);
+            });
+        });
+
+        gpio.open(7, "output", function(err) {
+            gpio.write(7, 1, function() {
+                gpio.close(7);
+            });
+        });
+
+    }, 10000);
+
+    gpio.open(5, "output", function(err) {
+        gpio.write(5, 0, function() {
+            gpio.close(5);
+        });
+    });
+
+    gpio.open(7, "output", function(err) {
+        gpio.write(7, 0, function() {
+            gpio.close(7);
+        });
+    });
+
+
+});
--- pi/controllers/socketController.js
+++ pi/controllers/socketController.js
@@ -1,11 +1,27 @@
-var app = require('../app');
+var app = require('../app'),
+    piContrller = require('./piController');
 
 module.exports.init = function(socket) {
 
     console.log('connected');
 
-    app.socket.on('update', function(data){
+    app.socket.on('update', function(data) {
+
         console.log(data);
+
+        switch (data.type) {
+
+            case 'fb':
+                piContrller.facebook();
+                break;
+            case 'drib':
+                piContrller.dribble();
+                break;
+            case 'tweet':
+                piContrller.twitter();
+                break;
+        }
+
     });
 
 };
